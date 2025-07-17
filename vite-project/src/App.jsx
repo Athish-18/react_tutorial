63d79fc6eb6cbda4f7f444d88b06bb9b91f9f8e7
Athish <cathishk@gmail.com>
2025-07-17 15:46:21 +0530

    event handlers

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,59 @@
-import React, { useState } from 'react'
-import Card from "./Card.jsx"
-function App() {
-  // Create , manage , handle all state
-  const[name,setName]=useState(" ")
+import React from 'react'
+const App = () => {
+  /*
+  function handleClick()
+  {
+    alert("U clicked");
+  }
   return (
-    <div>
-      <Card name={name} setName={setName}> </Card>
-      <p> I m the parent component and value of name is {name} </p>
-    </div>
-  )
+    <button  onClick={handleClick}> Please click me</button>
+  ) */
+
+
+    /*  return (
+      <btton onClick={alert("i got clicked")} // dont do immediate invocation runs only once 
+       <button  onClick={()=>{alert("i got clicked")}}>  // this is req to work and avoid the issue above
+         Please click me
+       </button>
+     ); */
+
+
+     /* function handleMouseover()
+     {
+      alert("u hovered over me")
+     }
+
+     return(
+     <p onMouseOver={handleMouseover}> I m a para</p> ) */
+
+
+     /* function handleInputChange(e)
+     {
+      console.log("value changed in console");
+      console.log("value till now", e.target.value);
+     }
+
+     return(
+      <form >
+        <input type="text" onChange={handleInputChange}></input>
+      </form>
+     )
+
+ */
+
+
+    /*  function handleSubmit(e) {
+       e.preventDefault(); // prevent page reload
+       alert("Shall I submit?");
+     }
+
+     return (
+       <form onSubmit={handleSubmit}>
+         <button type="submit">Submit</button>
+       </form>
+     ); */
+
+
 }
 
 export default App
