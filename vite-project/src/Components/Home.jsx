d26134e0d3222b26872c40212fd4eb901005a65a
Athish <cathishk@gmail.com>
2025-07-29 23:33:00 +0530

    added code

--- vite-project/src/Components/Home.jsx
+++ vite-project/src/Components/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+
+  const navigate=useNavigate();
+
+  const handleClick = () => {
+    navigate('/about');// Navigate to the About page;
+  };
+
   return (
-    <div>Home</div>
+    <div>Home
+      <button onClick={handleClick}>
+        Click Me
+      </button>
+    </div>
   )
 }
 
