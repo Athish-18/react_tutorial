4f4122fe9b2d249a3bcee604780e4bbf3060d588
Athish <cathishk@gmail.com>
2025-07-31 18:53:41 +0530

    StopWatch using UseRef

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,30 +1,40 @@
-import React, { useRef } from 'react'
-import {useState,useEffect,UseRef} from 'react'
-import { set } from 'react-hook-form';
+import React, { useRef } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  let val = useRef(0);
+  // Stopwatch state
+  const [timer, setTimer] = useState(0);
+  let timeRef = useRef(null);
 
-  let btnref = useRef(); // Created a ref to the button in order to change its color ie  // increment button will change its color when change color of first button is clicked
+  // Start timer
+ const startTimer = () => {
+   if (!timeRef.current) {
+     timeRef.current = setInterval(() => { // Increment timer every second, see the flow u click on start button and function start fn called and set interval fn is called in interval of 1 sec -- ie timeref.current stores the id of the interval if not 0 starts from 1 ie 1,2,3 . When stop clicked the timeref current is cleared and the timer stops incrementing ie set to 0 using clear interval  /// UseRef is needed to persist the interval ID across renders qithout it the interval id would reset on every render therefore we use useRef to store the interval id
+       setTimer((timer) => timer + 1);
+     }, 1000);
+   }
+ };
 
-  function handleClick() {
-    val.current = val.current + 1;
-    console.log("val of val", val.current);
-    setCount(count + 1);
-  }
-  function changeColor() {
-    btnref.current.style.backgroundColor = "red"; // Changes the color of the button to red
-  }
+  // Stop timer
+  const stopTimer = () => {
+    clearInterval(timeRef.current);
+    timeRef.current = null;
+  };
+
+  // Reset timer
+  const resetTimer = () => {
+    stopTimer();
+    setTimer(0);
+  };
 
   return (
     <div>
-      <button ref={btnref} onClick={handleClick}>
-        Click me
-      </button>
-
-      <button onClick={changeColor}>Change color of first button</button>
+      <h2>Stopwatch</h2>
+      <div style={{ fontSize: "2rem", margin: "1rem 0" }}>{timer} s</div>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+      <button onClick={resetTimer}>Reset</button>
     </div>
   );
-}
-export default App
+};
+export default App;
