8eb901e672a9b0124d0697c61d008d50d89b977a
Athish <cathishk@gmail.com>
2025-07-31 18:24:38 +0530

    UseRef use case 2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,24 +3,28 @@ import {useState,useEffect,UseRef} from 'react'
 import { set } from 'react-hook-form';
 
 const App = () => {
-  const [count,setCount]=useState(0); 
-  let val=useRef(0);// useRef is used to persist values across renders without causing re-renders ie value is stored and increased across re renders. , returns a object with current property in which value is stored ie if i click button 5 times, val.current will be 5 without it it would re render val would be 1 1 1  
+  const [count, setCount] = useState(0);
+  let val = useRef(0);
+
+  let btnref = useRef(); // Created a ref to the button in order to change its color ie  // increment button will change its color when change color of first button is clicked
 
   function handleClick() {
-    val.current=val.current+1;
-    console.log('val of val', val.current);
-    setCount(count+1);
-  
+    val.current = val.current + 1;
+    console.log("val of val", val.current);
+    setCount(count + 1);
   }
+  function changeColor() {
+    btnref.current.style.backgroundColor = "red"; // Changes the color of the button to red
+  }
+
   return (
     <div>
-      <button onClick={handleClick}>Click me</button>
+      <button ref={btnref} onClick={handleClick}>
+        Click me
+      </button>
 
-      <div>
-        Count {count}
-      </div>
+      <button onClick={changeColor}>Change color of first button</button>
     </div>
-  )
+  );
 }
-
 export default App
