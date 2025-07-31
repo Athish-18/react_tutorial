c8463dd57a90440b6e96a5c6a2ee95da18d45d69
Athish <cathishk@gmail.com>
2025-07-31 10:58:50 +0530

    added code

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,10 @@ import About from "./Components/About";
 import Dashboard from "./Components/Dashboard";
 import Navbar from "./Components/Navbar";
 import ParamComp from "./Components/ParamComp";
+import Courses from "./Components/courses";
+import MockTests from "./Components/MockTests";
+import Reports from "./Components/reports";
+import Notfound from "./Components/Notfound";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -32,6 +36,20 @@ const router = createBrowserRouter([
         <Dashboard />
       </div>
     ),
+    children: [
+      {
+        path: "courses",
+        element: <Courses />,
+      },
+      {
+        path: "mock-tests",
+        element: <MockTests />,
+      },
+      {
+        path: "reports",
+        element: <Reports/>,
+      },
+    ],
   },
   {
     path: "/student/:id",
@@ -42,6 +60,10 @@ const router = createBrowserRouter([
       </div>
     ),
   },
+  {
+    path: "*",
+    element:<Notfound />
+  }
 ]);
 
 const App = () => {
---
+++ vite-project/src/Components/Mocktests.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MockTests = () => {
+  return (
+    <div>Mock Tests</div>
+  )
+}
+
+export default MockTests
